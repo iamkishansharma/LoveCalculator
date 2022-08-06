a6474c2f782f6b7cf9b6fa26de4f5d0f6743a4ca
Kishan Kr Sharma <kishan.sharma@wiseyak.com>
2022-08-06 10:37:39 +0545

    Initial Commit

---
+++ LoveCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoveCalculator
+//
+//  Created by Kishan Kr Sharma on 8/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoveCalculator/LoveCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveCalculatorApp.swift
+//  LoveCalculator
+//
+//  Created by Kishan Kr Sharma on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct LoveCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
